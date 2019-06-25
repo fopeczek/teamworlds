2ab3666fa61728787af76668e115b39ec161f812
marvin <mw1996oede@googlemail.com>
2019-06-25 15:05:17 +0200

    moved map.cpp to map.h and used it as implementation (as usual)

--- src/engine/shared/map.cpp
+++ src/engine/shared/map.cpp
@@ -1,34 +1,14 @@
-/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
-/* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <base/system.h>
-#include <engine/map.h>
+#include "map.h"
 #include <engine/storage.h>
-#include <game/mapitems.h>
-#include "datafile.h"
+#include <engine/kernel.h>
 
-class CMap : public IEngineMap
-{
-	CDataFileReader m_DataFile;
-public:
-	CMap() {}
 
-	virtual void *GetData(int Index) { return m_DataFile.GetData(Index); }
-	virtual void *GetDataSwapped(int Index) { return m_DataFile.GetDataSwapped(Index); }
-	virtual void UnloadData(int Index) { m_DataFile.UnloadData(Index); }
-	virtual void *GetItem(int Index, int *pType, int *pID) { return m_DataFile.GetItem(Index, pType, pID); }
-	virtual void GetType(int Type, int *pStart, int *pNum) { m_DataFile.GetType(Type, pStart, pNum); }
-	virtual void *FindItem(int Type, int ID) { return m_DataFile.FindItem(Type, ID); }
-	virtual int NumItems() { return m_DataFile.NumItems(); }
-
-	virtual void Unload()
+bool CMap::Load(const char *pMapName, IKernel *pKernel, IStorage *pStorage)
 	{
-		m_DataFile.Close();
-	}
-
-	virtual bool Load(const char *pMapName, IStorage *pStorage)
-	{
+		if(!pKernel)
+			return false;
 		if(!pStorage)
-			pStorage = Kernel()->RequestInterface<IStorage>();
+			pStorage = pKernel->RequestInterface<IStorage>();
 		if(!pStorage)
 			return false;
 		if(!m_DataFile.Open(pStorage, pMapName, IStorage::TYPE_ALL))
@@ -52,7 +32,7 @@ public:
 				if(pLayer->m_Type == LAYERTYPE_TILES)
 				{
 					CMapItemLayerTilemap *pTilemap = reinterpret_cast<CMapItemLayerTilemap *>(pLayer);
-					
+
 					if(pTilemap->m_Version > 3)
 					{
 						const int TilemapCount = pTilemap->m_Width * pTilemap->m_Height;
@@ -85,26 +65,7 @@ public:
 					}
 				}
 			}
-			
+
 		}
-		
-		return true;
-	}
-
-	virtual bool IsLoaded()
-	{
-		return m_DataFile.IsOpen();
-	}
-
-	virtual SHA256_DIGEST Sha256()
-	{
-		return m_DataFile.Sha256();
-	}
-
-	virtual unsigned Crc()
-	{
-		return m_DataFile.Crc();
-	}
-};
-
-extern IEngineMap *CreateEngineMap() { return new CMap; }
+	return true;
+}
